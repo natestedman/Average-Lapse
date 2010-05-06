9fa7746c72b422859eae4e3c46d67ff68ca09c07
Nate Stedman <natesm@gmail.com>
2010-05-06 00:24:14 -0400

    Initial commit

---
+++ ALWindowController.h
@@ -0,0 +1,16 @@
+//
+//  ALWindowController.h
+//  AverageLapse
+//
+//  Created by Nate Stedman on 5/6/10.
+//  Copyright 2010 Nate Stedman. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface ALWindowController : NSWindowController {
+    IBOutlet NSView* mainView;
+}
+
+@end
---
+++ ALWindowController.m
@@ -0,0 +1,14 @@
+//
+//  ALWindowController.m
+//  AverageLapse
+//
+//  Created by Nate Stedman on 5/6/10.
+//  Copyright 2010 Nate Stedman. All rights reserved.
+//
+
+#import "ALWindowController.h"
+
+
+@implementation ALWindowController
+
+@end
